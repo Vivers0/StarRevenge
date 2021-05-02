c8f9deef8221a7ee0b296525265041a57690e694
Vivers1 <vivers.savva@gmail.com>
2021-05-02 09:11:12 +0300

    edit module fetch user in warn.js

--- src/lib/admin/warn.js
+++ src/lib/admin/warn.js
@@ -12,8 +12,7 @@ class Warn extends Command {
 
     async exec(message) {
         const [_, ...args] = message.content.slice("!".length).split(/ +/)
-        const roles = ['641325497753534484', '773859511588225034', '672063251537723412', '704235358304337970',
-        '672059478568599573', '672063138505555988', '641916470745497601', '719311800125882382', '694848466077089914'];
+        const roles = ['838112472766218270'];
         let cache = 0
         message.member.roles.cache.array().map(el => {
             roles.map(e => {
@@ -22,12 +21,11 @@ class Warn extends Command {
                 }
             })
         })
-        console.log(cache)
         if (cache === 0) return message.channel.send('Недостаточно прав!')
 
         if (!message.guild.me.hasPermission('BAN_MEMBERS')) return message.channel.send('Недостаточно прав!')
 
-        const member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]))
+        const member = message.guild.member(message.mentions.users.first()) || await message.guild.members.fetch(args[0]) 
         if(!member) return message.reply(`Пользователь не найден.`)
 
         let reason = args[1] || 'Отсуствует';
